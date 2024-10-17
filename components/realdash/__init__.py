e52dc8b952d2197931f5dddd522f14851a07ebc1
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-17 23:30:00 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.realdash import CONF_SB
+
 
 realdash_ns = cg.esphome_ns.namespace("realdash")
 REALDASH = realdash_ns.class_("REALDASH", cg.Component)
@@ -19,6 +19,6 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.SEB(config[CONF_SB]))
+    cg.add(var.set_SEB(config[CONF_SB]))
     
 
