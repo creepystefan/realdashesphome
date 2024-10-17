354652e181774c44bb7561b67b2cc9369d893c4d
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-17 23:07:43 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -4,12 +4,12 @@ from esphome.const import CONF_ID
 
 realdash_ns = cg.esphome_ns.namespace("realdash")
 REALDASH = realdash_ns.class_("REALDASH", cg.Component)
-SB = 'seB'
+CONF_SB = 'SEB'
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(REALDASH),
-        cv.Optional(SB, default=115200): cv.int_,
+        cv.Optional(CONF_SB, default=115200): cv.int_,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -18,5 +18,10 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.seB(config[SB]))
+    cg.add(var.SEB(config[CONF_SB]))
     
+
+
+CONF_1 = '2'
+cv.Optional(CONF_1, default=10): cv.int_,
+  cg.add(var.set_1(config[CONF_2]))
