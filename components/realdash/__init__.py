55dd291ce1e8e4fbf7ad6375666b8ee44a22af42
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-17 22:38:58 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -4,12 +4,12 @@ from esphome.const import CONF_ID
 
 realdash_ns = cg.esphome_ns.namespace("realdash")
 REALDASH = realdash_ns.class_("REALDASH", cg.Component)
-BAUDRATE_RD = 'SERIALBAUDRATE'
+SERIALBAUDRATE = 'baudrate_rd'
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(REALDASH),
-        cv.Optional(BAUDRATE_RD, default=115200): cv.int_,
+        cv.Optional(SERIALBAUDRATE, default=115200): cv.int_,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
