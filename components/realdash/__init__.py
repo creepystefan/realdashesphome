3e0fb207d98196d16667a9e005b3718adc1be2aa
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-17 21:33:45 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -2,12 +2,12 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-real_dash_ns = cg.esphome_ns.namespace("realdash")
+REALDASH_ns = cg.esphome_ns.namespace("realdash")
 Real_Dash = realdash_ns.class_("Real_Dash", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(real_dash),
+        cv.GenerateID(): cv.declare_id(REALDASH),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
