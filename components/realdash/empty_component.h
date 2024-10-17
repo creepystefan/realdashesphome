8b006103e5598e841fa15b7270035bd9fbdec155
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-17 19:27:38 +0200

    Update empty_component.h

--- components/realdash/empty_component.h
+++ components/realdash/empty_component.h
@@ -23,6 +23,34 @@ class EmptyComponent : public Component {
   delay(5);
 }
 
+void ReadDigitalStatuses()
+{
+  digitalPins = 0;
+  int bitposition = 0;
+  for (int i=1; i<4; i++)
+  {
+    if (digitalRead(i) == HIGH) digitalPins |= (1 << bitposition);
+    bitposition++;
+  }
+}
+void ReadAnalogStatuses()
+{
+  for (int i=0; i<5; i++)
+  {
+    analogPins[i] = analogRead(i);
+  }
+}
+
+
+void SendCANFramesToSerial()
+{
+  byte buf[8];
+  memcpy(buf, &digitalPins, 2);
+  memcpy(buf + 2, &analogPins[0], 2);
+  memcpy(buf + 4, &analogPins[1], 2);
+  memcpy(buf + 6, &analogPins[2], 2);
+  SendCANFrameToSerial(FRAME_ID1, buf);
+}
  
 void dump_config() override;
 };
