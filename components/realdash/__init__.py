c8623cd26d5263e09a7c24ddfeb8fd9079c30fe4
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-18 00:38:08 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -19,4 +19,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_current_resolution(config[CONF_CURRENT_RESOLUTION]))
+    cg.add(var.set_BAUDRATESERIAL(config[CONF_BAUDRATESERIAL]))
