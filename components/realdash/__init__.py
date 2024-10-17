f0b99a26b97a677ae5744be1ecb740f40983e0eb
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-17 22:33:03 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -1,13 +1,27 @@
+CONF_MY_REQUIRED_KEY = 'my_required_key'
+CONF_MY_OPTIONAL_KEY = 'my_optional_key'
+
+CONFIG_SCHEMA = cv.Schema({
+  cv.Required(CONF_MY_REQUIRED_KEY): cv.string,
+  cv.Optional(CONF_MY_OPTIONAL_KEY, default=10): cv.int_,
+}).extend(cv.COMPONENT_SCHEMA)
+
+
+
+
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
 realdash_ns = cg.esphome_ns.namespace("realdash")
 REALDASH = realdash_ns.class_("REALDASH", cg.Component)
+BAUDRATE_RD = 'baudrate_realdash'
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(REALDASH),
+        cv.Optional(BAUDRATE_RD, default=115200): cv.int_,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
