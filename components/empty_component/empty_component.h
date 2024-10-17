30c6818a23b6e6455cf355716ef551623913c024
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-17 19:52:33 +0200

    Update empty_component.h

--- components/empty_component/empty_component.h
+++ components/empty_component/empty_component.h
@@ -15,16 +15,15 @@ int analogPins[7] = {0};
 
 class EmptyComponent : public Component {
  public:
-  void setup() override;
   
+void setup() override;
 {
   Serial.begin(SERIALBAUDRATE);
   delay(100);
 }
 
-  void loop() override;
+void loop() override;
 {
-
   ReadDigitalStatuses();
   ReadAnalogStatuses();
   SendCANFramesToSerial();
@@ -74,5 +73,4 @@ void dump_config() override;
 }  // namespace empty_component
 }  // namespace esphome
 
-}  // namespace empty_component
-}  // namespace esphome
+
