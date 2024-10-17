8bc9c9029a387848b922de0a418c028b3343875a
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-17 22:34:00 +0200

    Update realdash.h

--- components/realdash/realdash.h
+++ components/realdash/realdash.h
@@ -10,7 +10,7 @@ unsigned int digitalPins = 0;
 int analogPins[7] = {0};
         
 #define FRAME_ID1           0x81c
-#define SERIALBAUDRATE      115200
+//#define SERIALBAUDRATE      115200
 
 
 class REALDASH : public Component {
