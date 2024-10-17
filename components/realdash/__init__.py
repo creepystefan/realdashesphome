cf4bc674fccbbbab755030b2122af134d6855ed3
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-18 00:56:57 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -19,4 +19,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_BAUDRATESERIAL(config[CONF_BAUDRATESERIAL]))
+    
