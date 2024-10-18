bd91bb67a7fc41b1bfd6f2b317050680139057e5
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-18 02:25:43 +0200

    Update realdash.h

--- components/44/realdash/realdash.h
+++ components/44/realdash/realdash.h
@@ -50,15 +50,15 @@ void ReadAnalogStatuses()
 }
 
 
-void SendCANFramesToSerial()
-{
-  byte buf[8];
-  memcpy(buf, &digitalPins, 2);
-  memcpy(buf + 2, &analogPins[0], 2);
-  memcpy(buf + 4, &analogPins[1], 2);
-  memcpy(buf + 6, &analogPins[2], 2);
-  SendCANFrameToSerial(FRAME_ID1, buf);
-}
+//void SendCANFramesToSerial()
+//{
+//  byte buf[8];
+//  memcpy(buf, &digitalPins, 2);
+//  memcpy(buf + 2, &analogPins[0], 2);
+//  memcpy(buf + 4, &analogPins[1], 2);
+//  memcpy(buf + 6, &analogPins[2], 2);
+//  SendCANFrameToSerial(FRAME_ID1, buf);
+//}
 
 void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
 {
