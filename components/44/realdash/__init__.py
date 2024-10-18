ce00b9587fe5f611a5d6cfcde501705840780e86
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-18 02:47:29 +0200

    Update __init__.py

--- components/44/realdash/__init__.py
+++ components/44/realdash/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-
+from .. import 44_ns
 MULTI_CONF = True
 
 realdash_ns = cg.esphome_ns.namespace("realdash")
