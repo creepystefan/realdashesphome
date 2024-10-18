0e7f51c2966218503ce3ff7685ac30661f8e2b41
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-18 02:47:42 +0200

    Update __init__.py

--- components/44/__init__.py
+++ components/44/__init__.py
@@ -3,20 +3,4 @@ import esphome.codegen as cg
 empty_fan_ns = cg.esphome_ns.namespace('44')
 
 
-44_ns = cg.esphome_ns.namespace("44")
-44 = realdash_ns.class_("441", cg.Component)
-
-
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(44),
-        
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
 
