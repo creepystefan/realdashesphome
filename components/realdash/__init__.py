671546e79f790906fe4b52f06eb00d1fe9788d4c
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-20 00:10:34 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -9,7 +9,7 @@ CONF_RD_BAUDRATE = 'rd_baudrate'
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(REALDASH),
-        cv.Optional(CONF_BD_RAEALDASH, default=115200): cv.string,
+        cv.Optional(CONF_RD_BAUDRATE, default=115200): cv.string,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
