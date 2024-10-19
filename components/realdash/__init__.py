2893bd8bd736e91562b86d4acb94bf99365d6fc6
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-19 23:24:33 +0200

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-CONF_BAUD_RATE = 'baud_rate'
+CONF_REAL_BAUD_RATE = 'rdbaudrate'
 
 realdash_ns = cg.esphome_ns.namespace("realdash")
 REALDASH = realdash_ns.class_("REALDASH", cg.Component)
@@ -9,7 +9,7 @@ REALDASH = realdash_ns.class_("REALDASH", cg.Component)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(REALDASH),
-        cv.Optional(CONF_BAUD_RATE, default=11520): cv.int_,
+        cv.Optional(CONF_REAL_BAUD_RATE, default=11520): cv.int_,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -18,5 +18,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-
+ cg.add(var.set_rdbaudrate(config[CONF_REAL_BAUD_RATE]))
     
