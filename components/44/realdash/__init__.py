efbfea1c9f2221fc2401b1bce231066d2da04fc6
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-20 22:10:25 +0200

    Update __init__.py

--- components/44/realdash/__init__.py
+++ components/44/realdash/__init__.py
@@ -1,1 +1,23 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
 
+realdash_ns = cg.esphome_ns.namespace("realdash")
+REALDASH = realdash_ns.class_("REALDASH", cg.Component)
+CONF_FRAME_ID1 = 'rd_frame_id1'
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        
+        cv.Optional(CONF_FRAME_ID1, default=0x81d): cv.int_,
+                
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    cg.add_define("rd_baudrate", config[CONF_RD_BAUDRATE])
+    cg.add_define("rd_frame_id1", config[CONF_FRAME_ID1])
