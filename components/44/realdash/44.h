d89b9110d8df52e41e678b36ec42692f1f2c2a11
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-20 22:05:49 +0200

    Update 44.h

--- components/44/realdash/44.h
+++ components/44/realdash/44.h
@@ -1,1 +1,31 @@
+#pragma once
 
+#include "esphome/core/component.h"
+#include "esphome.h"
+
+namespace esphome {
+namespace 44 { 
+
+
+unsigned int digitalPins = 0;
+int analogPins[7] = {0};
+        
+class REALDASH : public Component {
+ public:
+
+
+void SendCANFramesToSerial()
+{
+  byte buf[8];
+  memcpy(buf, &digitalPins, 2);
+  memcpy(buf + 2, &analogPins[0], 2);
+  memcpy(buf + 4, &analogPins[1], 2);
+  memcpy(buf + 6, &analogPins[2], 2);
+  SendCANFrameToSerial(rd_frame_id1, buf);
+}
+
+//void dump_config() override
+};
+
+}  // namespace 44
+}  // namespace esphome
