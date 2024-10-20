cb98bde2446c492592b908d30eaa5c69b661f8bb
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-20 22:06:02 +0200

    Update 44.h

--- components/44/realdash/44.h
+++ components/44/realdash/44.h
@@ -21,7 +21,7 @@ void SendCANFramesToSerial()
   memcpy(buf + 2, &analogPins[0], 2);
   memcpy(buf + 4, &analogPins[1], 2);
   memcpy(buf + 6, &analogPins[2], 2);
-  SendCANFrameToSerial(rd_frame_id1, buf);
+  SendCANFrameToSerial(rd_frame_id, buf);
 }
 
 //void dump_config() override
