b25077c8e8374b1b5799803793b24f3eb727d53e
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-20 22:20:50 +0200

    Update __init__.py

--- components/44/realdash/__init__.py
+++ components/44/realdash/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+from .. import empty_fan_ns
 
 realdash_ns = cg.esphome_ns.namespace("realdash")
 REALDASH = realdash_ns.class_("REALDASH", cg.Component)
