0fda5879f560e3d441a91aa75347e77b0b5836d7
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-21 16:01:24 +0200

    Update output.py

--- components/realdash44/output.py
+++ components/realdash44/output.py
@@ -3,15 +3,15 @@ import esphome.config_validation as cv
 from esphome.components import output
 from esphome.const import CONF_ID
 
-realdash44_ns = cg.esphome_ns.namespace("realdash44")
-
-CanId = realdash44_ns.class_(
-    "CanID", output.BinarOutput, cg.Component
+empty_binary_output_ns = cg.esphome_ns.namespace("empty_binary_output")
+EmptyBinaryOutput = empty_binary_output_ns.class_(
+    "EmptyBinaryOutput", output.BinaryOutput, cg.Component
 )
 
-CONFIG_SCHEMA = output.Binary_Output_SCHEMA.extend(
+CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend(
     {
-        cv.Required(CONF_ID): cv.declare_id(CanID),
+        cv.Required(CONF_ID): cv.declare_id(EmptyBinaryOutput),
+        
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -20,4 +20,3 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await output.register_output(var, config)
     await cg.register_component(var, config)
-    
