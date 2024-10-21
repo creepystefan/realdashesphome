eaf1f44c6f55858d5609fed0b8a8f0047572617d
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-21 12:49:20 +0200

    Update realdash.py

--- components/44/realdash.py
+++ components/44/realdash.py
@@ -3,14 +3,14 @@ import esphome.config_validation as cv
 from esphome.components import realdash
 from esphome.const import CONF_ID
 
-frame_44_ns = cg.esphome_ns.namespace("frame_44")
-FramE44 = frame_44_ns.class_(
-    "FramE44", realdash.44, cg.Component
+44_ns = cg.esphome_ns.namespace("44")
+Frame44 = 44_ns.class_(
+    "Frame44", realdash.44, cg.Component
 )
 
 CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend(
     {
-        cv.Required(CONF_ID): cv.declare_id(FramE44),
+        cv.Required(CONF_ID): cv.declare_id(Frame44),
         cv.Required(CONF_FRAME_ID): cv.int_,
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -18,6 +18,6 @@ CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await realdash.register_realdash(var, config)
+    await output.register_realdash(var, config)
     await cg.register_component(var, config)
     cg.add_define("rd_frame_id", config[CONF_FRAME_ID])
