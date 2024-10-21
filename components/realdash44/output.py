854e2c69b69dc91d97b5bba3d2a987e4a1df13a4
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-21 15:40:49 +0200

    Update output.py

--- components/realdash44/output.py
+++ components/realdash44/output.py
@@ -4,11 +4,12 @@ from esphome.components import output
 from esphome.const import CONF_ID
 
 realdash44_ns = cg.esphome_ns.namespace("realdash44")
+
 CANID = realdash44_ns.class_(
-    "CANID", output.CANID, cg.Component
+    "CANID", output.CAN_ID, cg.Component
 )
 
-CONFIG_SCHEMA = output.CANID_SCHEMA.extend(
+CONFIG_SCHEMA = output.CAN_ID_SCHEMA.extend(
     {
         cv.Required(CONF_ID): cv.declare_id(CANID),
     }
