de87d2164d8f8c7d77179df369a866941577396a
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-21 08:49:14 +0200

    Update __init__.py

--- components/44/realdash/__init__.py
+++ components/44/realdash/__init__.py
@@ -1,23 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-
 from esphome.const import (
     CONF_ID,
 )
 
 from .. import 44_ns
-
-Realdash44 = template_ns.class_(
-    "TemplateSensor", realdash.Realdash, cg.PollingComponent
-
-
-realdash_ns = cg.esphome_ns.namespace("realdash")
-CONF_44 = 44_ns.class_("CONF_44", cg.Component)
+Realdash44 = template_ns.class_("Realdash44", realdash.Realdash, cg.PollingComponent)
 CONF_FRAME_ID1 = 'rd_frame_id1'
 
-EmptyFan = 44_ns.class_("EmptyFan", cg.Component, realdash.REALDASH)
 
-CONFIG_SCHEMA = realdash.REALDASH_SCHEMA.extend(
+CONFIG_SCHEMA = realdash.REALDASH_SCHEMA(
     {
         cv.GenerateID(): cv.declare_id(44),
         cv.Optional(CONF_FRAME_ID, default=0x81d): cv.int_,
@@ -30,4 +22,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add_define("rd_frame_id1", config[CONF_FRAME_ID])
+    cg.add_define("rd_frame_id", config[CONF_FRAME_ID])
