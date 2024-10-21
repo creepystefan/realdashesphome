ce0d624f16ffed0dd009a19382cdcbe05210086d
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-21 16:15:14 +0200

    Update output.py

--- components/empty_binary_output/output.py
+++ components/empty_binary_output/output.py
@@ -20,3 +20,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await output.register_output(var, config)
     await cg.register_component(var, config)
+
+    cg.add_define("rd_frame_id1", config[EmptyBinaryOutput])
