b45f448dbdc6dd3e2a3e7cd08ab75521811cc579
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-22 07:59:20 +0200

    Update output.py

--- components/empty_binary_output/output.py
+++ components/empty_binary_output/output.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import output
 from esphome.const import CONF_ID
-CONF_FRAME_ID = 'rd_frame_id'
 
 empty_binary_output_ns = cg.esphome_ns.namespace("empty_binary_output")
 EmptyBinaryOutput = empty_binary_output_ns.class_(
@@ -20,5 +19,3 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await output.register_output(var, config)
     await cg.register_component(var, config)
-
-   
