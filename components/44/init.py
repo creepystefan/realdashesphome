46ea30671983fc3579953ddfb9691ab48b070652
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-22 10:30:22 +0200

    Update init.py

--- components/44/init.py
+++ components/44/init.py
@@ -1,25 +1,12 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.const import (
-    CONF_ID,
-)
-
-from .. import 44_ns
-Realdash44 = 44_ns.class_("Realdash44", realdash.Realdash, cg.PollingComponent)
-CONF_FRAME_ID = 'rd_frame_id'
-
-
-CONFIG_SCHEMA = realdash.REALDASH_SCHEMA(
-    {
-        cv.GenerateID(): cv.declare_id(44),
         cv.Optional(CONF_FRAME_ID, default=0x81d): cv.int_,
-                
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
     cg.add_define("rd_frame_id", config[CONF_FRAME_ID])
+
+ //  TemplateBinaryOutput = template_ns.class_("TemplateBinaryOutput", output.BinaryOutput)
+     //  TemplateFloatOutput = template_ns.class_("TemplateFloatOutput", output.FloatOutput)
+     //  TemplateBinaryOutput = RealDash44Output
+     //  TemplateFloatOutput  = RealDash44Float
+     //  BinaryOutput         = Output44
+     //  FloatOutput          = Float44
+     //  FLOAT_OUTPUT         = FLOAT_44
+     //  BINARY_OUTPUT        = BINARY_44
+     //  CONF_WRITE_ACTION    = 
