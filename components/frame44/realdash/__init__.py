07250959acd49521c9193459da054839b17ba1f2
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 21:54:29 +0200

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -1,14 +1,4 @@
 
-TemplateButton = template_ns.class_("TemplateButton", button.Button)
-
-CONFIG_SCHEMA = button.button_schema(TemplateButton)
-
-
-async def to_code(config):
-    await button.new_button(config)
-
-
-#################################################################
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
