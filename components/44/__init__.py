0547670dc34020cc6d6b4f4408d4b20d30493b99
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 08:14:14 +0200

    Update __init__.py

--- components/44/__init__.py
+++ components/44/__init__.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-CONF_FRAME_ID1 = 'rd_frame_id'
+CONF_FRAME_ID = 'rd_frame_id'
 
 frame44_ns = cg.esphome_ns.namespace("frame44")
 FRAME44 = frame44_ns.class_("FRAME44", cg.Component)
