a3404ef4c1a541e1c87ad6ed057db013cfab7153
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 07:49:09 +0200

    Update __init__.py

--- components/44/__init__.py
+++ components/44/__init__.py
@@ -12,37 +12,14 @@ RealDash44Float = 44_ns.class_("RealDash44Float", output.Float44)
 CONF_FLOAT = "float"
 CONF_WRITE_ACTION = "write_action"
 
-CONFIG_SCHEMA = cv.typed_schema(
+CONF_FRAME_ID1 = 'rd_frame_id1'
+
+CONFIG_SCHEMA = cv.Schema(
     {
-        CONF_BINARY: output.BINARY_44_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(RealDash44Output),
-                cv.Required(CONF_WRITE_ACTION): automation.validate_automation(
-                    single=True
-                ),
-            }
-        ),
-        CONF_FLOAT: output.FLOAT_44_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(RealDash44Float),
-                cv.Required(CONF_WRITE_ACTION): automation.validate_automation(
-                    single=True
-                ),
-            }
-        ),
-    },
-    lower=True,
-)
+        cv.GenerateID(): cv.declare_id(REALDASH),
+        cv.Optional(CONF_FRAME_ID1, default=0x81c): cv.int_,
+                
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    if config[CONF_TYPE] == CONF_BINARY:
-        await automation.build_automation(
-            var.get_trigger(), [(bool, "state")], config[CONF_WRITE_ACTION]
-        )
-    else:
-        await automation.build_automation(
-            var.get_trigger(), [(float, "state")], config[CONF_WRITE_ACTION]
-        )
-    await output.register_output(var, config)
