d98c05a885d8fc2aaae3deffeaa68e12d455c748
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 08:10:33 +0200

    Update __init__.py

--- components/44/__init__.py
+++ components/44/__init__.py
@@ -1,16 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from .. import realdash_ns
 
 CONF_FRAME_ID1 = 'rd_frame_id'
 
-frame_ns = cg.esphome_ns.namespace("frame")
-FRAME = frame_ns.class_("FRAME", cg.Component)
+frame44_ns = cg.esphome_ns.namespace("frame44")
+FRAME44 = frame44_ns.class_("FRAME44", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(FRAME),
+        cv.GenerateID(): cv.declare_id(FRAME44),
         cv.Optional(CONF_FRAME_ID, default=0x81c): cv.int_,
                 
     }
