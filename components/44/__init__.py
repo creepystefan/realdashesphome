5a28eb82bb5e4bd23989fab14cbff82531fdbe86
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 07:50:55 +0200

    Update __init__.py

--- components/44/__init__.py
+++ components/44/__init__.py
@@ -1,25 +1,27 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from .. import realdash_ns
 from esphome.const import CONF_ID
-from .. import template_ns
 from .. import realdash_ns
 
 
 RealDash44Output = 44_ns.class_("RealDash44Output", output.Output44)
 RealDash44Float = 44_ns.class_("RealDash44Float", output.Float44)
 
-CONF_FLOAT = "float"
-CONF_WRITE_ACTION = "write_action"
-
-CONF_FRAME_ID1 = 'rd_frame_id1'
+CONF_FRAME_ID1 = 'rd_frame_id'
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(REALDASH),
-        cv.Optional(CONF_FRAME_ID1, default=0x81c): cv.int_,
+        cv.Optional(CONF_FRAME_ID, default=0x81c): cv.int_,
                 
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    cg.add_define("rd_frame_id", config[CONF_FRAME_ID])
+    
 
