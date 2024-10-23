40cb5f5d3538f3638a5085551db43cf7c1de024e
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 17:06:58 +0200

    Create __init__.py

---
+++ components/btserial/__init__.py
@@ -0,0 +1,25 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+IS_PLATFORM_COMPONENT = True
+
+realdash_ns = cg.esphome_ns.namespace("realdash")
+REALDASH = realdash_ns.class_("REALDASH", cg.Component)
+CONF_RD_BAUDRATE = 'rd_baudrate'
+CONF_FRAME_ID1 = 'rd_frame_id1'
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(REALDASH),
+        cv.Optional(CONF_FRAME_ID1, default=0x81c): cv.int_,
+                
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    cg.add_define("rd_frame_id1", config[CONF_FRAME_ID1])
