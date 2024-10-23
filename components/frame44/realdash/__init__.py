478fc1f43a5ef31763929cb8d38a6858aa67a060
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 21:29:43 +0200

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -15,7 +15,7 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Optional(CONF_FRAME_ID, default=0x81c): cv.int_,
                 
     }
-).extend(cv.COMPONENT_SCHEMA)
+).extend(cv.frame44.frame44_SCHEMA)
 
 
 async def to_code(config):
