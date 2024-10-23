3614b9104c96c40cc646d86857f7e72332d75d28
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 21:34:43 +0200

    Update frame44_realdash.h

--- components/frame44/realdash/frame44_realdash.h
+++ components/frame44/realdash/frame44_realdash.h
@@ -1,5 +1,29 @@
 #pragma once
 
+#include "esphome/components/button/button.h"
+
+namespace esphome {
+namespace template_ {
+
+class TemplateButton : public button::Button {
+ public:
+  // Implements the abstract `press_action` but the `on_press` trigger already handles the press.
+  void press_action() override{};
+};
+
+}  // namespace template_
+}  // namespace esphome
+
+
+
+
+
+
+
+
+
+#pragma once
+
 #include "esphome/core/component.h"
 #include "esphome.h"
 
