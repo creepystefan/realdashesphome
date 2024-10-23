8604aed5dacec0e97967a4af3e58807ed862b997
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 21:30:24 +0200

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID
 from .. import frame44_ns
 
-TemplateButton = template_ns.class_("TemplateButton", button.Button)
+
 frame44_ns = cg.esphome_ns.namespace("frame44")
 FRAME44 = frame44_ns.class_("FRAME44", frame44.frame44)
 
