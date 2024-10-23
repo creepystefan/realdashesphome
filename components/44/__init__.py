5906833b6d3b37ca7583cdfa81c0052185757325
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 08:03:47 +0200

    Update __init__.py

--- components/44/__init__.py
+++ components/44/__init__.py
@@ -5,11 +5,12 @@ from .. import realdash_ns
 
 CONF_FRAME_ID1 = 'rd_frame_id'
 
-RealDash44Output = 44_ns.class_("RealDash44Output", output.Output44)
+frame_ns = cg.esphome_ns.namespace("frame")
+FRAME = frame_ns.class_("FRAME", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(RealDash44Output),
+        cv.GenerateID(): cv.declare_id(FRAME),
         cv.Optional(CONF_FRAME_ID, default=0x81c): cv.int_,
                 
     }
