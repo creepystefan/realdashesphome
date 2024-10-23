7811c3547e1ea6194e1c99360251e70b7fab3dd9
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-23 08:21:02 +0200

    Update __init__.py

--- components/frame44/__init__.py
+++ components/frame44/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+from .. import realdash_ns
 
 frame44_ns = cg.esphome_ns.namespace("frame44")
 FRAME44 = frame44_ns.class_("FRAME44", cg.Component)
