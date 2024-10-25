dce332eddde76ecf7e736f2d56977703f719e47d
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-25 11:55:27 +0200

    Update btserial.h

--- components/btserial/btserial.h
+++ components/btserial/btserial.h
@@ -16,7 +16,7 @@ BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin(btserial); //Name des ESP32
+  SerialBT.begin(realdash); //Name des ESP32
   Serial.println("Der ESP32 ist bereit. Verbinde dich nun über Bluetooth.");
 }
 
