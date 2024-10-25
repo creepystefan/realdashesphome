1671e821bd7a893aca5a62bbe9f7029be424e2da
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-25 12:53:30 +0200

    Update frame44.h

--- components/frame44/frame44.h
+++ components/frame44/frame44.h
@@ -20,9 +20,9 @@ void SendCANFramesToSerial()
 {
   byte buf[8];
   memcpy(buf, &digitalPins, 2);
-  memcpy(buf + 2, &analogPins[0], 2);
-  memcpy(buf + 4, &analogPins[1], 2);
-  memcpy(buf + 6, &analogPins[2], 2);
+  memcpy(buf + 2, 2);
+  memcpy(buf + 4, 2);
+  memcpy(buf + 6, 2);
   SendCANFrameToSerial(0xc33, buf);
 }
 
