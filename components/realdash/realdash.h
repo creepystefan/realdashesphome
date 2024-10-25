901064c64cfc8399f4ce3781eed2f40d521799b9
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-25 08:38:16 +0200

    Update realdash.h

--- components/realdash/realdash.h
+++ components/realdash/realdash.h
@@ -19,43 +19,43 @@ void setup() override
   delay(100);
 }
 
-//void loop() override
-//{
-//  ReadDigitalStatuses();
-//  ReadAnalogStatuses();
-//  SendCANFramesToSerial();
-//  delay(5);
+void loop() override
+{
+  ReadDigitalStatuses();
+  ReadAnalogStatuses();
+  SendCANFramesToSerial();
+  delay(5);
   SendCANFrameToSerial();
-//}
+}
 
-//void ReadDigitalStatuses()
-//{
-//  digitalPins = 0;
-//  int bitposition = 0;
-//  for (int i=1; i<4; i++)
-//  {
-//    if (digitalRead(i) == HIGH) digitalPins |= (1 << bitposition);
-//    bitposition++;
-//  }
-//}
-//void ReadAnalogStatuses()
-//{
-//  for (int i=0; i<5; i++)
-//  {
-//    analogPins[i] = analogRead(i);
-//  }
-//}
+void ReadDigitalStatuses()
+{
+  digitalPins = 0;
+  int bitposition = 0;
+  for (int i=1; i<4; i++)
+  {
+    if (digitalRead(i) == HIGH) digitalPins |= (1 << bitposition);
+    bitposition++;
+  }
+}
+void ReadAnalogStatuses()
+{
+  for (int i=0; i<5; i++)
+  {
+    analogPins[i] = analogRead(i);
+  }
+}
 
 
-//void SendCANFramesToSerial()
-//{
- // byte buf[8];
- // memcpy(buf, &digitalPins, 2);
- // memcpy(buf + 2, &analogPins[0], 2);
- // memcpy(buf + 4, &analogPins[1], 2);
- // memcpy(buf + 6, &analogPins[2], 2);
- // SendCANFrameToSerial(0xc55, buf);
-//}
+void SendCANFramesToSerial()
+{
+  byte buf[8];
+  memcpy(buf, &digitalPins, 2);
+  memcpy(buf + 2, &analogPins[0], 2);
+  memcpy(buf + 4, &analogPins[1], 2);
+  memcpy(buf + 6, &analogPins[2], 2);
+  SendCANFrameToSerial(0xc55, buf);
+}
 
 void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
 {
