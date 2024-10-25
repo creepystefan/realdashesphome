7016c3a35e580b37ffa8baabb2ff437d1d723e19
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-25 12:43:11 +0200

    Update frame44.h

--- components/frame44/frame44.h
+++ components/frame44/frame44.h
@@ -6,38 +6,15 @@
 namespace esphome {
 namespace frame44 { 
 
-unsigned int digitalPins = 0;
-int analogPins[7] = {0};
-
 class FRAME44 : public Component {
  public:
 
 void loop() override
 {
-  ReadDigitalStatuses();
-  ReadAnalogStatuses();
   SendCANFramesToSerial();
   delay(5);
 }
 
-void ReadDigitalStatuses()
-{
-  digitalPins = 0;
-  int bitposition = 0;
-  for (int i=1; i<4; i++)
-  {
-    if (digitalRead(i) == HIGH) digitalPins |= (1 << bitposition);
-    bitposition++;
-  }
-}
-
-void ReadAnalogStatuses()
-{
-  for (int i=0; i<5; i++)
-  {
-    analogPins[i] = analogRead(i);
-  }
-}
 
 void SendCANFramesToSerial()
 {
@@ -46,7 +23,7 @@ void SendCANFramesToSerial()
   memcpy(buf + 2, &analogPins[0], 2);
   memcpy(buf + 4, &analogPins[1], 2);
   memcpy(buf + 6, &analogPins[2], 2);
-  SendCANFrameToSerial(rd_frame_id, buf);
+  SendCANFrameToSerial(0xc33, buf);
 }
 
 };
