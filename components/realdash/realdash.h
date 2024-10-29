ca0ecdfa2b3f4e3a2037b99326196dd78de8aa98
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-29 01:13:24 +0100

    Create realdash.h

---
+++ components/realdash/realdash.h
@@ -0,0 +1,24 @@
+void setup() 
+{
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  SendCANFrameToSerial();
+  delay(5);
+}
+
+
+
+void SendCANFrameToSerial()
+{
+  const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };
+  const byte canID[4] = { 0x00, 0x00, 0x0c, 0x11 };
+  const byte Data[8] = { 0x00, 0x00, 0x0c, 0x11, 0x00, 0x00, 0x0c, 0x11 };
+  Serial.write(serialBlockTag, 4);
+  Serial.write(canID, 4);
+  Serial.write(Data, 8);
+  
+}
+
