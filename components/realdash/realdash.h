c25a364676763d9a334411920a54ec96b4098d9e
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-30 00:22:18 +0100

    Update realdash.h

--- components/realdash/realdash.h
+++ components/realdash/realdash.h
@@ -18,27 +18,10 @@ void setup() override
 void loop() override
 {
  SendFrame();
- SendCANFramesToSerial();
  delay(5); 
 }
 
-void SendCANFramesToSerial()
-{
-  byte buf[8];
-  memcpy(buf, &0x00, 2);
-  memcpy(buf + 2, &0x01, 2);
-  memcpy(buf + 4, &0x02, 2);
-  memcpy(buf + 6, &0x03, 2);
-  SendCANFrameToSerial(0x1, buf);
-}
 
-void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
-{
-  const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };
-  Serial.write(serialBlockTag, 4);
-  Serial.write((const byte*)&canFrameId, 4);
-  Serial.write(frameData, 8);
-}
 
 void SendFrame()
 {
