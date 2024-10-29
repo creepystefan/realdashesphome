90a6a5083082405fda41399a7a86f0da061fac8c
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-29 23:51:23 +0100

    Update realdash.h

--- components/realdash/realdash.h
+++ components/realdash/realdash.h
@@ -17,6 +17,11 @@ void setup() override
  
 void loop() override
 {
+ SendFrame();
+}
+
+void SendFrame()
+{
   const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };
   const byte canID[4] = { 0x1 };
   const byte Data[8] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
@@ -25,6 +30,7 @@ void loop() override
   Serial.write(Data, 8);
   delay(5);
 }
+
 };
 }  // namespace realdash
 }  // namespace esphome
