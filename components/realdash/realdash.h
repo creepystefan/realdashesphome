2ccc8d7bb593d1767cfb0e99227e192b49ee25ae
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-29 02:36:26 +0100

    Update realdash.h

--- components/realdash/realdash.h
+++ components/realdash/realdash.h
@@ -7,24 +7,32 @@
 namespace esphome {
 namespace realdash { 
 
+int button = D0;                      // button is connected to D0
+
 class REALDASH : public Component {
  public:
 void setup() override
 {
   Serial.begin(115200);
+  pinMode(button, INPUT_PULLDOWN);    // sets pin as input
 }
 
 void loop() override
 {
   const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };
   const byte canID[4] = { (can_id) };
-  const byte Data[8] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+  while(digitalRead(button) == HIGH)
+   {
+    const byte Data[8] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x00 };          // sets the LED on
+    delay(200);                                                                      // waits for 200mS
+    const byte Data[8] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0xFF };           // sets the LED off
+    delay(200);                                                                         // waits for 200mS
+   }
   Serial.write(serialBlockTag, 4);
   Serial.write(canID, 4);
   Serial.write(Data, 8);
   delay(5);
 }
 };
-
 }  // namespace realdash
 }  // namespace esphome
