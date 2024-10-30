4194788eefe7d102406c8e64bae79f54733485cf
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-31 00:24:54 +0100

    Update realdash.h

--- components/realdash/realdash.h
+++ components/realdash/realdash.h
@@ -17,16 +17,8 @@ void setup() override
  void loop() override
 {
   
- const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };                  // fix Frame Start to READ 
+  const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };                  // fix Frame Start to READ 
   const byte canID[4] = { 0x22c2 };                                             // CAN_ID Variable
-  const byte Data[8] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };    // DATA Frame Variable
-  Serial.write(serialBlockTag, 4);                                           
-  Serial.write(canID, 4);
-  Serial.write(Data, 8);
- delay(5); 
-
- const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };                  // fix Frame Start to READ 
-  const byte canID[4] = { 0x22c1 };                                             // CAN_ID Variable
   const byte Data[8] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };    // DATA Frame Variable
   Serial.write(serialBlockTag, 4);                                           
   Serial.write(canID, 4);
