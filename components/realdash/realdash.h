b8cddc4b03aa0bee645004d9946d6eb32aa660e4
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-31 00:31:13 +0100

    Update realdash.h

--- components/realdash/realdash.h
+++ components/realdash/realdash.h
@@ -16,6 +16,13 @@ void setup() override
 
 
 
+void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
+{
+  const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };
+  Serial.write(serialBlockTag, 4);
+  Serial.write((const byte*)&canFrameId, 4);
+  Serial.write(frameData, 8);
+}
 
 };
 }  // namespace realdash
