781d007cf60901454e5e3abf05ada87b38073eef
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-04 22:07:11 +0100

    Update frame44_realdash.h

--- components/frame44/realdash/frame44_realdash.h
+++ components/frame44/realdash/frame44_realdash.h
@@ -1,1 +1,13 @@
 
+#pragma once
+
+namespace esphome {
+namespace frame44_ {
+
+class Frame44Realdash : public realdash::Realdash {
+ public:
+  
+};
+
+}  // namespace frame44_
+}  // namespace esphome
