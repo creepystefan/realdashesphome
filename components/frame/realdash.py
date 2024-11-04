5cc9e89d379ce39bd94740e3f79900ad62a9576a
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-04 23:28:37 +0100

    Update realdash.py

--- components/frame/realdash.py
+++ components/frame/realdash.py
@@ -3,7 +3,7 @@ from .components import realdash
 from esphome.const import CONF_ID
 
 frame_ns = cg.esphome_ns.namespace('frame')
-Frame = frame_ns.class_('Frame' cg.Component)
+Frame = frame_ns.class_('Frame', cg.Component)
 
 CONFIG_SCHEMA = realdash.FLOAT_OUTPUT_SCHEMA.extend({
     cv.Required(CONF_ID): cv.declare_id(Frame),
