b08df98be594d6f52d278cf990a38f8af128884a
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-04 23:26:38 +0100

    Update and rename frame.py to realdash.py

---
+++ components/frame/realdash.py
@@ -0,0 +1,14 @@
+import esphome.codegen as cg
+from .components import realdash
+from esphome.const import CONF_ID
+
+frame_ns = cg.esphome_ns.namespace('frame')
+Frame = frame_ns.class_('Frame' cg.Component)
+
+CONFIG_SCHEMA = realdash.FLOAT_OUTPUT_SCHEMA.extend({
+    cv.Required(CONF_ID): cv.declare_id(Frame),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
