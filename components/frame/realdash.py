f1fd898e43161b31ea4fc407f38cd0cd97051cad
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-06 22:28:27 +0100

    Update realdash.py

--- components/frame/realdash.py
+++ components/frame/realdash.py
@@ -1,5 +1,5 @@
 import esphome.codegen as cg
-from esphome.components import realdash
+from github_com.creepystefan.realdash-esphome import realdash
 from esphome.const import CONF_ID
 
 frame_ns = cg.esphome_ns.namespace('frame')
