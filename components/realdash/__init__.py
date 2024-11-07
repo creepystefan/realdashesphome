bdf3f6ce7487e71b7839552884ffa802bee14bd8
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-07 01:33:50 +0100

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -26,7 +26,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add_define("can_id", config[CONF_CAN_ID])
+    cg.add_define(0x"can_id", config[CONF_CAN_ID])
     
 
 
