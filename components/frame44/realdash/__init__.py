0f690a3362e0da9b4a6afb9663194b9d2ecda79e
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-07 20:59:40 +0100

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -1,11 +1,5 @@
 #import esphome.codegen as cg
 
-url = "https://gist.githubusercontent.com/operatorequals/ee5049677e7bbc97af2941d1d3f04ace/raw/e55fa867d3fb350f70b2897bb415f410027dd7e4"
-
-with httpimport.remote_repo(url):
-  import hello
-
-
 from .. import realdash_ns
 from .. import frame44_ns
 
