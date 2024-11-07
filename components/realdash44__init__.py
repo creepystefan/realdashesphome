f0679a2883ce5b67f7aeafda5f6152e3a377f6fe
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-07 22:59:06 +0100

    Update and rename components/realdashcan/__init__.py to components/realdash44__init__.py

--- components/realdash44__init__.py
+++ components/realdash44__init__.py
@@ -2,13 +2,13 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-realdashcan_ns = cg.esphome_ns.namespace("realdashcan")
-REALDASHCAN = realdashcan_ns.class_("REALDASHCAN", cg.Component)
+realdash44_ns = cg.esphome_ns.namespace("realdash44")
+REALDASH44 = realdash44_ns.class_("REALDASH44", cg.Component)
 CONF_CAN_ID = 'can_id'
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(REALDASH),
+        cv.GenerateID(): cv.declare_id(REALDASH44),
         cv.Required(CONF_CAN_ID): cv.int_,
                 
     }
