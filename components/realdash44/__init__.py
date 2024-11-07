9cf5df2f6675cacf32bbe98d84be7e83ce380f14
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-07 23:04:09 +0100

    Update __init__.py

--- components/realdash44/__init__.py
+++ components/realdash44/__init__.py
@@ -9,7 +9,7 @@ CONF_CAN_ID = 'can_id'
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(REALDASH44),
-        cv.Required(CONF_CAN_ID): cv.int_,
+  #      cv.Required(CONF_CAN_ID): cv.int_,
                 
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -19,5 +19,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add_define("can_id", config[CONF_CAN_ID])
+  #  cg.add_define("can_id", config[CONF_CAN_ID])
     
