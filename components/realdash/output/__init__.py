01cc70e6522a0cfca57f4ac60679d1ce6a582097
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-11 10:47:37 +0100

    Update __init__.py

--- components/realdash/output/__init__.py
+++ components/realdash/output/__init__.py
@@ -1,17 +1,4 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.const import CONF_ID
-from esphome.components import output
 
-from .. import realdash_ns
-
-RealdashOutput = realdash_ns.class_("RealdashOutput", output.Output)
-
-CONFIG_SCHEMA = button.button_schema(RealdashOutput)
-
-
-async def to_code(config):
-    await button.new_output(config)
 
 
 
