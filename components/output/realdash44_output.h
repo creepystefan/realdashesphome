f353fe0cf307ff928d11ac80e0fe752ca745889e
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-11 09:38:28 +0100

    Update realdash44_output.h

--- components/output/realdash44_output.h
+++ components/output/realdash44_output.h
@@ -1,11 +1,9 @@
 #pragma once
 
-#include "esphome/core/automation.h"
-#include "esphome/components/output/binary_output.h"
 #include "esphome/components/output/float_output.h"
 
 namespace esphome {
-namespace template_ {
+namespace realdash44_ {
 
 class TemplateBinaryOutput : public output::BinaryOutput {
  public:
@@ -17,15 +15,5 @@ class TemplateBinaryOutput : public output::BinaryOutput {
   Trigger<bool> *trigger_ = new Trigger<bool>();
 };
 
-class TemplateFloatOutput : public output::FloatOutput {
- public:
-  Trigger<float> *get_trigger() const { return trigger_; }
-
- protected:
-  void write_state(float state) override { this->trigger_->trigger(state); }
-
-  Trigger<float> *trigger_ = new Trigger<float>();
-};
-
-}  // namespace template_
+}  // namespace realdash44_
 }  // namespace esphome
