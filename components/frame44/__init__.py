89c161c6f5087880a652eed0985bc02573ce0211
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-11 12:17:06 +0100

    Update __init__.py

--- components/frame44/__init__.py
+++ components/frame44/__init__.py
@@ -1,3 +1,25 @@
-import esphome.codegen as cg
 
-frame44_ns = cg.esphome_ns.namespace("frame44_")
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+#IS_PLATFORM_COMPONENT = True
+
+realdash_ns = cg.esphome_ns.namespace("realdash")
+REALDASH = realdash_ns.class_("REALDASH", cg.Component)
+#CONF_CAN_ID = 'can_id'
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(REALDASH),
+   #     cv.Required(CONF_CAN_ID): cv.int_,
+                
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+  #  cg.add_define("can_id", config[CONF_CAN_ID])
