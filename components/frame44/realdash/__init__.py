592a409490905cb98d9eeb97bacaa3525f494757
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-11 08:13:51 +0100

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -1,5 +1,5 @@
 from realdash import realdash
-from . import frame44_ns
+from .. import frame44_ns
 
 Frame44Realdash = frame44_ns.class_("Frame44Realdash", realdash.Realdash)
 
@@ -9,3 +9,15 @@ CONFIG_SCHEMA = realdash.realdash_schema(Frame44Realdash)
 async def to_code(config):
     await realdash.new_realdash(config)
 
+
+
+
+from .. import template_ns
+
+TemplateButton = template_ns.class_("TemplateButton", button.Button)
+
+CONFIG_SCHEMA = button.button_schema(TemplateButton)
+
+
+async def to_code(config):
+    await button.new_button(config)
