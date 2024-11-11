855e726317f9989ac52b1e31f10c5a437e3c4757
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-11 11:51:26 +0100

    Update output.py

--- components/realdash44/output.py
+++ components/realdash44/output.py
@@ -5,7 +5,7 @@ from esphome.const import CONF_ID
 
 realdash44_ns = cg.esphome_ns.namespace("realdash44")
 Real_dash44 = realdash44_ns.class_(
-    "Real_dash44", output.BinaryOutput, cg.PollingComponent
+    "Real_dash44", output.BinaryOutput, cg.Component
    
 )
 
