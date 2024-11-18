dbbbc65633f36c6ae05b721b22766e30e3375fc1
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-18 21:59:49 +0100

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -6,12 +6,12 @@ from esphome.const import CONF_ID
 
 realdash_ns = cg.esphome_ns.namespace("realdash")
 REALDASH = realdash_ns.class_("REALDASH", cg.Component)
-#CONF_CAN_ID = 'can_id'
+CONF_CAN_ID = 'can_id'
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(REALDASH),
-   #     cv.Required(CONF_CAN_ID): cv.int_,
+        cv.Required(CONF_CAN_ID): cv.int_,
                 
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -21,7 +21,6 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    cg.add(var.set_update_interval(conf[CONF_INTERVAL]))
-    
-  #  cg.add_define("can_id", config[CONF_CAN_ID])
+    cg.add(var.set_SendCANFramesToSerial_realdash(conf[CONF_CAN_ID]))
+    cg.add_define("can_id", config[CONF_CAN_ID])
     
