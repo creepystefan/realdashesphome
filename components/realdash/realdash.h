a46a1236db5f534d33ed589891a11d07d53cc60c
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-22 00:30:34 +0100

    Update realdash.h

--- components/realdash/realdash.h
+++ components/realdash/realdash.h
@@ -1,3 +1,23 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome.h"
+
+
+namespace esphome {
+namespace realdash { 
+
+
+uint16_t data0 = 2815;
+uint8_t data1 = 10; 
+int data2 = 11; 
+unsigned int data3 = 10;  
+int can_id_;
+
+class REALDASH : public PollingComponent {
+ public:
+  REALDASH() : PollingComponent(10) {}
+
 void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
 {
   const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };
