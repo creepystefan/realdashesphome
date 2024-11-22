6fabae39f8deda63687df061323b06ac26a0c4bb
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-22 01:10:53 +0100

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -5,3 +5,12 @@ IS_PLATFORM_COMPONENT = True
 
 from .. import template_ns
 
+
+frame44_ns = cg.esphome_ns.namespace("frame44_")
+RealdashFrame44 = frame44_ns.class_("RealdashFrame44", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema();
+
+async def to_code(config):
+    await cg.register_component(var, config)
+    
