fc582f4944ed4581894eee60d958f41a26d32993
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-22 01:11:51 +0100

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -1,15 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from .. import realdash_ns
 
 IS_PLATFORM_COMPONENT = True
-
-from .. import template_ns
-
 
 frame44_ns = cg.esphome_ns.namespace("frame44_")
 RealdashFrame44 = frame44_ns.class_("RealdashFrame44", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema();
+CONFIG_SCHEMA = cv.Schema()
 
 async def to_code(config):
     await cg.register_component(var, config)
