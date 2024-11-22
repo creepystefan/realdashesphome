e0c2332971fcc8d5b1719f0effb9c3e95e6242a3
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-22 01:19:19 +0100

    Update __init__.py

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -2,11 +2,3 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 
 IS_PLATFORM_COMPONENT = True
-
-REALDASH = realdash_ns.class_("REALDASH", cg.Component)
-
-CONFIG_SCHEMA = cv.Schema()
-
-async def to_code(config):
-    await cg.register_component(var, config)
-    
