cdacbf0f813fec4089b3e2db1ddf1300d903e8a9
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-22 01:27:35 +0100

    Update __init__.py

--- components/realdashserial/__init__.py
+++ components/realdashserial/__init__.py
@@ -1,1 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
 
+realdashserial_ns = cg.esphome_ns.namespace("realdashserial")
+REALDASHSERIAL = realdashserial_ns.class_("REALDASHSERIAL", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(REALDASHSERIAL),
+              
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
+    
