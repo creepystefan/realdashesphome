64e3110898ed6419f966eebef5efe0c888efb410
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-25 06:56:14 +0100

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -1,32 +1,36 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
 from esphome.components import realdash
+from esphome.const import (
+    CONF_ID,
+    CONF_ADDRESS,
+)
 
 frame44_ns = cg.esphome_ns.namespace("frame44_")
 RealdashFrame44 = frame44_ns.class_("RealdashFrame44", cg.Component)
 #RealdashFrame44 = frame44_ns.class_("RealdashFrame44", realdash.Realdash)
-CONF_CAN_ID = 'can_id'
+#CONF_CAN_ID = 'can_id'
 
 CONFIG_SCHEMA = cv.Schema(
 #CONFIG_SCHEMA = realdash.realdash_schema(RealdashFrame44)
     {
         cv.GenerateID(): cv.declare_id(RealdashFrame44),
-        cv.Required(CONF_CAN_ID): cv.int_,
+        cv.Required(CONF_ADDRESS): cv.int_,
                 
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
-
 async def to_code(config):
     var = cg.new_Pvariable(
         config[CONF_ID],
-        config[CONF_CAN_ID],
+        #config[CONF_CAN_ID],
+        #config[CONF_ADDRESS],
     )
     await cg.register_component(var, config)
     #await button.new_button(config)
     
-#    cg.add(var.set_can_id(config[CONF_CAN_ID]))
+    #cg.add(var.set_can_id(config[CONF_CAN_ID]))
+    cg.add(var.set_can_id(config[CONF_ADDRESS]))
 
 
 
