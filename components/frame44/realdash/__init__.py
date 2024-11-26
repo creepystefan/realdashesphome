b04cdd200e0f9cc53e132f9d18f89fdad4ed1133
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-26 18:24:59 +0100

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -4,12 +4,12 @@ from esphome.components import realdash
 from esphome.const import (CONF_ID)
 
 frame44_ns = cg.esphome_ns.namespace("frame44_")
-RealdashFrame44 = frame44_ns.class_("RealdashFrame44", cg.Component)
-#RealdashFrame44 = frame44_ns.class_("RealdashFrame44", realdash.Realdash)
+#RealdashFrame44 = frame44_ns.class_("RealdashFrame44", cg.Component)
+RealdashFrame44 = frame44_ns.class_("RealdashFrame44", realdash.Realdash)
 CONF_CANID = 'canid'
 
-CONFIG_SCHEMA = cv.Schema(
-#CONFIG_SCHEMA = realdash.realdash_schema(RealdashFrame44)
+#CONFIG_SCHEMA = cv.Schema(
+CONFIG_SCHEMA = realdash.realdash_schema(RealdashFrame44)
     {
         cv.GenerateID(): cv.declare_id(RealdashFrame44),
         cv.Required(CONF_CANID): cv.int_,
@@ -20,7 +20,7 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(
         config[CONF_ID],
-        config[CONF_CANID],
+        #config[CONF_CANID],
         )
     await cg.register_component(var, config)
         
