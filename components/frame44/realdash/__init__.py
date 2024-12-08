f67af7537486e0df7023718a748e5901b3e41125
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-08 15:12:41 +0100

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -19,7 +19,7 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
    var = cg.new_Pvariable(
         config[CONF_ID],
-        config[CONF_CANID],
+        #config[CONF_CANID],
         )
     await cg.register_component(var, config)
         
