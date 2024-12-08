b1f80d3b7618ec9c9b4a76301ffa7ee7553de933
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-08 15:36:46 +0100

    Create test.h

---
+++ components/test/realdash/test.h
@@ -0,0 +1,23 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import realdash
+from esphome.const import (CONF_ID)
+
+test_ns = cg.esphome_ns.namespace("test")
+RealdashTest = frame44_ns.class_("RealdashFrame44", cg.Component)
+CONF_CANID = 'canid'
+
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(RealdashTEST),
+        cv.Required(CONF_CANID): cv.int_,
+                
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+   var = cg.new_Pvariable(config[CONF_ID])
+   await cg.register_component(var, config)
+        
+   cg.add(var.set_canid(config[CONF_CANID]))
