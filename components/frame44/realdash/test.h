fe6bd3b71ede7eacba43e5fee41dc1cf7424b39d
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-08 15:26:37 +0100

    Update test.h

--- components/frame44/realdash/test.h
+++ components/frame44/realdash/test.h
@@ -1,1 +1,7 @@
-
+void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
+{
+  const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };
+  Serial.write(serialBlockTag, 4);
+  Serial.write((const byte*)&canFrameId, 4);
+  Serial.write(frameData, 8);
+}
