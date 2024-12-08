2db7e10d3b2952dd2e2f2245e0912e628fcbbc9f
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-08 15:38:42 +0100

    Create test_realdash.h

---
+++ components/test/realdash/test_realdash.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome.h"
+
+
+namespace esphome {
+namespace test_ { 
+
+
+class RealdashTest : public PollingComponent {
+ public:
+  RealdashTest() : PollingComponent(10) {}
+
+uint16_t data0 = 2815;
+uint8_t data1 = 10; 
+int data2 = 11; 
+unsigned int data3 = 10;  
+int canid;
+
+void set_canid(int canid) { canid = canid; }
+
+void setup() override
+{
+  Serial.begin(115200);
+  
+}
+
+void update() override
+{
+  SendCANFramesToSerial();
+ if (data0++ > 10000)
+  {
+    data0 = 500;
+  }
+  if (data1++ > 10000)
+  {
+    data1 = 10;
+  }
+  if (data2++ > 10000)
+  {
+    data2 = 0;
+  }
+  if (data3++ > 10000)
+  {
+    data3 = 0;
+  }
+ delay(5);
+}
+//class RealdashFrame44 : public PollingComponent {
+// public:
+//  RealdashFrame44() : PollingComponent(10) {}
+void SendCANFramesToSerial()
+{
+  byte buf[8];
+  memcpy(buf, &data0, 2);
+  memcpy(buf + 2, &data1, 2);
+  memcpy(buf + 4, &data2, 2);
+  memcpy(buf + 6, &data3, 2);
+  SendCANFrameToSerial(canid, buf);
+}
+//};
+void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
+{
+  const byte serialBlockTag[4] = { 0x44, 0x33, 0x22, 0x11 };
+  Serial.write(serialBlockTag, 4);
+  Serial.write((const byte*)&canFrameId, 4);
+  Serial.write(frameData, 8);
+}
+};
+
+}  // namespace test_
+}  // namespace esphome
