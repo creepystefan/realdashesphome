6f8c95b79b539d42b361b747ebb117fa5440c81a
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-11 23:19:22 +0100

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -4,13 +4,14 @@ from esphome.components import realdash
 from esphome.const import (CONF_ID)
 
 frame44_ns = cg.esphome_ns.namespace("frame44_")
-RealdashFrame44 = frame44_ns.class_("RealdashFrame44", cg.Component)
+#RealdashFrame44 = frame44_ns.class_("RealdashFrame44", cg.Component)
+SendCANFrameToSerial = frame44_ns.class_("SendCANFrameToSerial", cg.Component)
 CONF_CANID = 'canid'
 
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(RealdashFrame44),
+        cv.GenerateID(): cv.declare_id(SendCANFrameToSerial),
         cv.Required(CONF_CANID): cv.int_,
                 
     }
