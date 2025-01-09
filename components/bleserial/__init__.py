44727de8672fa32af91ea638d99fa9bdaf8a43bf
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-09 22:39:44 +0100

    Update __init__.py

--- components/bleserial/__init__.py
+++ components/bleserial/__init__.py
@@ -2,6 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
+#Serial_ble
+
 bleserial_ns = cg.esphome_ns.namespace("bleserial")
 BLESERIAL = bleserial_ns.class_("BLESERIAL", cg.Component)
 
