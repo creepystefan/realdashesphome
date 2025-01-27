1eaa851ae66011bea06a3bab8bf7a2411a1d9783
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-27 07:55:32 +0100

    Update frame44_realdash.h

--- components/frame44/realdash/frame44_realdash.h
+++ components/frame44/realdash/frame44_realdash.h
@@ -40,8 +40,9 @@ void SendCANFramesToSerial()
   memcpy(buf + 2, &data2_, 2);
   memcpy(buf + 4, &data3_, 2);
   memcpy(buf + 6, &data4_, 2);
-  #RealdashFrame44::SendCANFrameToSerial(canid_, buf);
- REALDASH_TEST::SendCANFrameToSerial(canid_, buf);
+  //RealdashFrame44::SendCANFrameToSerial(canid_, buf);
+  REALDASH_TEST::SendCANFrameToSerial(canid_, buf);
+  SendCANFrameToSerial(canid_, buf);
 }
 
 //void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
