779e74b71b39ba3f4e927c7223a47ee1a8b17ae8
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-27 06:59:42 +0100

    Create __init__.py

---
+++ components/realdashtest/__init__.py
@@ -0,0 +1,21 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+realdashtest_ns = cg.esphome_ns.namespace("realdashtest")
+REALDASH_TEST = realdashtest_ns.class_("REALDASH_TEST", cg.Component)
+
+CONF_BTNAME = 'realdashtest'
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(REALDASH_TEST),
+        
+                
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
