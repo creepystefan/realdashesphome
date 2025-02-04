79e28d3e0e41d21096c3e02e5df211aaac7fcaab
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-04 22:09:01 +0100

    Update __init__.py

--- components/arealdashtest/__init__.py
+++ components/arealdashtest/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import realdashtest, uart
+from esphome.components import arealdashtest, uart
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['uart']
