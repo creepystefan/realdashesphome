0502903c4f0b3d1ab1a35240da78d720d2501532
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-05 23:22:54 +0100

    Update textsent.h

--- test/textsent.h
+++ test/textsent.h
@@ -8,7 +8,7 @@ void SendCANFramesToSerial()
 void SendTextExtensionFrameToSerial(unsigned long canFrameId, const char* text)
 {
   const byte textExtensionBlockTag[4] = { 0x55, 0x33, 0x22, 0x11 };
-  Serial.write(textExtensionBlockTag, 4);
-  Serial.write((const byte*)&canFrameId, 4);
-  Serial.write(text, strlen(text) + 1);
+  write_array(textExtensionBlockTag, 4);
+  write_array((const byte*)&canFrameId, 4);
+  write_array(text, strlen(text) + 1);
 }
