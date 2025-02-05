b23335ee7383161ee2793de2cd626603f548245d
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-05 22:40:06 +0100

    Update __init__.py

--- components/frame44/realdash/__init__.py
+++ components/frame44/realdash/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import arealdashtest
+from esphome.components import arealdashtest, uart
 from esphome.const import (CONF_ID)
 
 AUTO_LOAD = ["arealdashtest"]
