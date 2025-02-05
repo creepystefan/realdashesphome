9f600067facde556bb816de9aea38fde211e2e98
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-05 22:38:58 +0100

    Update frame44_realdash.h

--- components/frame44/realdash/frame44_realdash.h
+++ components/frame44/realdash/frame44_realdash.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome.h"
-//#include "esphome/components/uart/uart.h"
+#include "esphome/components/uart/uart.h"
 
 namespace esphome {
 namespace frame44_ { 
