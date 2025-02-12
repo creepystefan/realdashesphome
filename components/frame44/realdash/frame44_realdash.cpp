32e9647eea604a3c5799d4e9954b264e54dd98ca
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-12 15:15:23 +0100

    Update frame44_realdash.cpp

--- components/frame44/realdash/frame44_realdash.cpp
+++ components/frame44/realdash/frame44_realdash.cpp
@@ -18,22 +18,5 @@ void RealdashFrame44::dump_config(){
     ESP_LOGCONFIG(TAG, "Frame44");
 }
 
-void XY6020Sensor::dump_config() {
-  ESP_LOGCONFIG(TAG, "XY6020:");
-  ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
-  LOG_SENSOR("  ", "seted Output Voltage", this->seted_output_voltage_sensor_);
-  LOG_SENSOR("  ", "seted Output Current", this->seted_output_current_sensor_);
-  LOG_SENSOR("  ", "Output Voltage", this->output_voltage_sensor_);
-  LOG_SENSOR("  ", "Output Current", this->output_current_sensor_);
-  LOG_SENSOR("  ", "Output Power", this->output_current_sensor_);
-  LOG_SENSOR("  ", "Input Voltage", this->input_voltage_sensor_);
-  LOG_SENSOR("  ", "Temperature Intern", this->temperature_intern_sensor_);
-  LOG_SENSOR("  ", "Temperature Extern", this->temperature_extern_sensor_);
-  LOG_SENSOR("  ", "Model", this->model_sensor_);
-  LOG_SENSOR("  ", "Software Version", this->software_version_sensor_);  
-}
-
-
-
 }  // namespace frame44_
 }  // namespace esphome
