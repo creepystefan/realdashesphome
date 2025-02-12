5b99843aa74a082982e4b8fc83d992f4c27e79f6
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-12 15:08:01 +0100

    Create frame44_realdash.cpp

---
+++ components/frame44/realdash/frame44_realdash.cpp
@@ -0,0 +1,22 @@
+#include "esphome/core/log.h"
+#include "frame44_realdash.h"
+
+namespace esphome {
+namespace frame44_ {
+
+static const char *TAG = "Frame44_Realdash";
+
+//void RealdashFrame44::setup() {
+
+//}
+
+//void RealdashFrame44::loop() {
+
+//}
+
+void RealdashFrame44::dump_config(){
+    ESP_LOGCONFIG(TAG, "Frame44");
+}
+
+}  // namespace frame44_
+}  // namespace esphome
