ad9fe9299dad8cc8f1abcc30e8ae4f4f2567861b
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-17 10:53:29 +0100

    Update frame44_realdash.cpp

--- components/frame44/realdash/frame44_realdash.cpp
+++ components/frame44/realdash/frame44_realdash.cpp
@@ -7,16 +7,13 @@ namespace frame44_ {
 static const char *TAG = "Realdash";
 
 //void RealdashFrame44::setup() {
-
 //}
 
 //void RealdashFrame44::loop() {
-
 //}
 
 void RealdashFrame44::dump_config(){
     ESP_LOGCONFIG(TAG, "Frame44-ID: 0x%08X", this->canid_);
-//    ESP_LOGD("custom", "Starting washer cycle!", this->data1_);
 }
 
 }  // namespace frame44_
