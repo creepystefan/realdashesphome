c6f6e948d92f6b3a9158e9aad02301788c60b1ea
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-18 22:49:45 +0100

    Update frame55_realdash.h

--- components/frame55/realdash/frame55_realdash.h
+++ components/frame55/realdash/frame55_realdash.h
@@ -11,17 +11,8 @@ class RealdashFrame55 : public uart::UARTDevice, public PollingComponent {
   RealdashFrame55() : PollingComponent(5000) {}
 
 uint32_t canid_;
-uint16_t data1_;
-uint16_t data2_;
-uint16_t data3_;
-uint16_t data4_;
 
 void set_canid(uint32_t canid) { this->canid_ = canid; }
-void set_data1(uint16_t data1) { this->data1_ = data1; }
-void set_data2(uint16_t data2) { this->data2_ = data2; }
-void set_data3(uint16_t data3) { this->data3_ = data3; }
-void set_data4(uint16_t data4) { this->data4_ = data4; }
-
 
 void setup() override
 {
@@ -30,26 +21,11 @@ void setup() override
 
 void update() override
 {
-  SendCANFramesToSerial();
-  delay(5);
 }
-
 
 void SendCANFramesToSerial()
 {
-  byte buf[8];
-  memcpy(buf, &data1_, 2);
-  memcpy(buf + 2, &data2_, 2);
-  memcpy(buf + 4, &data3_, 2);
-  memcpy(buf + 6, &data4_, 2);
-  RealdashFrame44::SendCANFrameToSerial(canid_, buf);
- 
-  ESP_LOGI("custom", "Frame44-ID: 0x%08X", this->canid_);
-  ESP_LOGI("custom", "DATA1:      0x%02X", this->data1_);
-  ESP_LOGI("custom", "DATA2:      0x%02X", this->data2_);
-  ESP_LOGI("custom", "DATA3:      0x%02X", this->data3_);
-  ESP_LOGI("custom", "DATA4:      0x%02X", this->data4_);
-
+  SendTextExtensionFrameToSerial(3203, "Das ist nur ein Test");
 }
 
 void SendTextExtensionFrameToSerial(unsigned long canFrameId, const char* text)
