5a63ad8feba093e22372aeb5c143b0d74fd16aa8
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-18 22:28:57 +0100

    Create frame55_realdash.h

---
+++ components/frame55/realdash/frame55_realdash.h
@@ -0,0 +1,40 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import (CONF_ID)
+
+#AUTO_LOAD = ["arealdashtest"]
+DEPENDENCIES = ['uart']
+
+frame55_ns = cg.esphome_ns.namespace("frame55_")
+RealdashFrame55 = frame55_ns.class_("RealdashFrame55", cg.PollingComponent, uart.UARTDevice)
+
+CONF_CANID = 'canid'
+CONF_DATA1 = 'data1'
+CONF_DATA2 = 'data2'
+CONF_DATA3 = 'data3'
+CONF_DATA4 = 'data4'
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(RealdashFrame55),
+        cv.Required(CONF_CANID): cv.int_range(min=0, max=0x1FFFFFFF),
+        cv.Optional(CONF_DATA1, default=0): cv.int_range(min=0, max=65535),
+        cv.Optional(CONF_DATA2, default=0): cv.int_range(min=0, max=65535),
+        cv.Optional(CONF_DATA3, default=0): cv.int_range(min=0, max=65535),
+        cv.Optional(CONF_DATA4, default=0): cv.int_range(min=0, max=65535),
+        
+    }
+#).extend(cv.COMPONENT_SCHEMA)
+).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+
+async def to_code(config):
+   var = cg.new_Pvariable(config[CONF_ID])
+   await cg.register_component(var, config)
+   await uart.register_uart_device(var, config)
+
+   cg.add(var.set_canid(config[CONF_CANID]))
+   cg.add(var.set_data1(config[CONF_DATA1]))
+   cg.add(var.set_data2(config[CONF_DATA2]))
+   cg.add(var.set_data3(config[CONF_DATA3]))
+   cg.add(var.set_data4(config[CONF_DATA4]))
