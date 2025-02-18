a4cc830674a1a79d54da2fe3f30aee0ebca9bd59
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-18 22:41:27 +0100

    Update frame55_realdash.h

--- components/frame55/realdash/frame55_realdash.h
+++ components/frame55/realdash/frame55_realdash.h
@@ -52,12 +52,12 @@ void SendCANFramesToSerial()
 
 }
 
-void SendCANFrameToSerial(unsigned long canFrameId, const byte* frameData)
+void SendTextExtensionFrameToSerial(unsigned long canFrameId, const char* text)
 {
-  const byte serialBlockTag[4] = { 0x55, 0x33, 0x22, 0x11 };
-  write_array(serialBlockTag, 4);
+  const byte textExtensionBlockTag[4] = { 0x55, 0x33, 0x22, 0x11 };
+  write_array(textExtensionBlockTag, 4);
   write_array((const byte*)&canFrameId, 4);
-  write_array(frameData, 8);
+  write_array(text, strlen(text) + 1);
 }
 
 void dump_config() override;
