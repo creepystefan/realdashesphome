e89395309c4aa6d18634800de59f1e74f81f007c
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-24 08:01:32 +0100

    Update frame55_realdash.h

--- components/frame55/realdash/frame55_realdash.h
+++ components/frame55/realdash/frame55_realdash.h
@@ -28,11 +28,11 @@ void SendCANFramesToSerialtext()
   SendTextExtensionFrameToSerial(canid_, "Das ist nur ein Test");
 }
 
-void SendTextExtensionFrameToSerial(unsigned long canFrameId, const char* text)
+void SendTextExtensionFrameToSerial(unsigned long canFrameIdtext, const char* text)
 {
   const byte textExtensionBlockTag[4] = { 0x55, 0x33, 0x22, 0x11 };
   write_array(textExtensionBlockTag, 4);
-  write_array((const byte*)&canFrameId, 4);
+  write_array((const byte*)&canFrameIdtext, 4);
   write_array(text, strlen(text) + 1);
 }
 
