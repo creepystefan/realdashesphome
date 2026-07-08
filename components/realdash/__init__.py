1bfe61ad3e9bf23271274c434107c7b29f6406e0
creepystefan <147045262+creepystefan@users.noreply.github.com>
2026-07-08 20:27:24 +0200

    Fix CONF_ID2 and update sensor registration logic

--- components/realdash/__init__.py
+++ components/realdash/__init__.py
@@ -11,7 +11,7 @@ from esphome.const import (
 
 
 CONF_CANID = 'canid'
-CONF_ID2 = 'canid'
+CONF_ID2 = 'id2'
 
 DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@creepystefan"]
@@ -21,10 +21,12 @@ realdash_ns = cg.esphome_ns.namespace("realdash")
 Realdash = realdash_ns.class_("Realdash", cg.Component, uart.UARTDevice)
 RealdashDevice = realdash_ns.class_("RealdashDevice")
 
+
 CONFIG_SENSORS_SCHEMA = cv.Schema(
     {
         cv.Required(CONF_ID): cv.use_id(sensor.Sensor),
-        #cv.Required(CONF_NAME): cv.string_strict,
+        cv.Optional(CONF_ID2): cv.use_id(sensor.Sensor),
+        #cv.Optional(CONF_ID2): validate_use_ID2,
         cv.Required(CONF_CANID): cv.int_range(min=0, max=0xFFFFFFFF),
     }
 )
@@ -32,7 +34,6 @@ CONFIG_SENSORS_SCHEMA = cv.Schema(
 CONFIG_BINARY_SENSORS_SCHEMA = cv.Schema(
     {
         cv.Required(CONF_ID): cv.use_id(binary_sensor.BinarySensor),
-        #cv.Required(CONF_NAME): cv.string_strict,
         cv.Required(CONF_CANID): cv.int_range(min=0, max=0xFFFFFFFF),
     }
 )
@@ -47,6 +48,7 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 
+
 async def to_code(config):
    var = cg.new_Pvariable(config[CONF_ID])
    await cg.register_component(var, config)
@@ -54,18 +56,19 @@ async def to_code(config):
 
    for sensor_cfg in config.get(CONF_SENSORS, []):
         s = await cg.get_variable(sensor_cfg[CONF_ID])
-        cg.add(var.register_sensor(sensor_cfg[CONF_CANID], s))
-        #cg.add(var.register_sensor(sensor_cfg[CONF_NAME], s))
-        #cg.add(var.register_sensor(s))
-        #cg.add(var.set_canid(sensor_cfg[CONF_CANID]))
+        if CONF_ID2 in sensor_cfg:
+            s2 = await cg.get_variable(sensor_cfg[CONF_ID2])
+            cg.add(var.register_sensor(sensor_cfg[CONF_CANID], s, s2))
+        if CONF_ID2 not in sensor_cfg:
+            cg.add(var.register_sensor(sensor_cfg[CONF_CANID], s))
         
+    
+   
 
    for sensor_cfg in config.get(CONF_BINARY_SENSORS, []):
         s = await cg.get_variable(sensor_cfg[CONF_ID])
-        cg.add(var.register_binary_sensor(sensor_cfg[CONF_CANID], sensor_cfg[CONF_CANID2], s))
-        #cg.add(var.register_binary_sensor(sensor_cfg[CONF_NAME], s))
-        #cg.add(var.set_canid(sensor_cfg[CONF_CANID]))
-        #cg.add(var.register_binary_sensor(s))
+        cg.add(var.register_binary_sensor(sensor_cfg[CONF_CANID], s))
+        
 
 async def register_realdash_device(var, config):
     parent = await cg.get_variable(config[CONF_REALDASH_ID])
